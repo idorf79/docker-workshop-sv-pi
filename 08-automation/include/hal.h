b5ddddabe36bff3e3e8f4c76afeb333a3e9fa278
Frido Steffers <frido.steffers@icloud.com>
2026-04-18 09:53:25 +0200

    removed old 08-automation

--- 08-automation/include/hal.h
+++
@@ -1,7 +0,0 @@
-#ifndef HAL_H
-#define HAL_H
-
-void hal_init(void);
-void hal_gpio_write(int pin, int value);
-
-#endif
--- 08-automation/run.bash
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-if [ ! -f output/firmware.elf ]; then
-    echo "Firmware not built. Run ./build.sh first."
-    exit 1
-fi
-
-echo "Running firmware..."
-./output/firmware.elf
